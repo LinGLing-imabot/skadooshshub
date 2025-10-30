32def49dc9979b11a11ee9c045fbfadfa0eed6eb
LinGLing-imabot <Skanda.penujurI@gmail.com>
2025-10-29 22:16:57 -0500

    add scramjet proxy

---
+++ scramjet.config.js
@@ -0,0 +1,10 @@
+self.__scramjet$config = {
+    prefix: "/scramjet/service/",
+    codec: "xor",
+    config: "/scramjet.config.js",
+    bundle: "https://cdn.jsdelivr.net/npm/@mercuryworkshop/scramjet/dist/scramjet.bundle.js",
+    worker: "https://cdn.jsdelivr.net/npm/@mercuryworkshop/scramjet/dist/scramjet.worker.js",
+    client: "https://cdn.jsdelivr.net/npm/@mercuryworkshop/scramjet/dist/scramjet.client.js",
+    shared: "https://cdn.jsdelivr.net/npm/@mercuryworkshop/scramjet/dist/scramjet.shared.js",
+    sync: "https://cdn.jsdelivr.net/npm/@mercuryworkshop/scramjet/dist/scramjet.sync.js"
+};
